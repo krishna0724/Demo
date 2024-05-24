907d705ee0a08c0609280d986e472536ce7dc1f3
11abc <jhak992@gmail.com>
2024-05-24 13:27:25 +0530

    this is maven project

---
+++ src/main/java/com/accenter/team/accenturesports/Add.java
@@ -0,0 +1,16 @@
+package com.accenter.team.accenturesports;
+
+
+public class Add {
+	private String name;
+	private String id;
+	private String role;
+	public Add(String name,String id,String role) {
+		this.name = name;
+		this.id = id;
+		this.role = role;
+	}
+	public String details() {
+		return "Player name : "+name+" Id : "+id+" Role : "+role;
+	}
+}
---
+++ src/main/java/com/accenter/team/accenturesports/Remove.java
@@ -0,0 +1,15 @@
+package com.accenter.team.accenturesports;
+
+import java.util.HashSet;
+
+public class Remove {
+	public HashSet<String> remove(HashSet<String> players,String value){
+		HashSet<String> newplayers = new HashSet();
+		for(String player:players) {
+			if(!player.contains(value)) {
+				newplayers.add(player);
+			}
+		}
+		return newplayers;
+	}
+}
